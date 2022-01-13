40299e13e4231a07ec928cf3c181312861687994
a-sha0234 <asharma_31@hotmail.com>
2022-01-13 14:39:30 +0000

    add form handling in the form component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Card from "./components/Card";
 import Form from "./components/Form";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,40 @@
 import React from "react";
+
 export default function Form() {
-  return <div className="App"></div>;
+  //------------------------------------------------------------------------------------------
+  // Handle form
+  //------------------------------------------------------------------------------------------
+  const [cityName, setCityName] = React.useState({
+    nameOfCity: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCityName((prev) => {
+      return {
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Name of currency</label>
+      <input
+        type="text"
+        placeholder="City name"
+        name=" nameOfCity"
+        // this is a controlled component as the value is coming from the state
+        //Also had to use defaultValue which is only for the inital render: https://stackoverflow.com/questions/47012169/a-component-is-changing-an-uncontrolled-input-of-type-text-to-be-controlled-erro
+        //had to do this as I was having a problem with my controlled component changing to an uncontrolled component
+        defaultValue={cityName.nameOfCity}
+        onChange={handleChange}
+      ></input>
+      <button>Search</button>
+    </form>
+  );
 }
